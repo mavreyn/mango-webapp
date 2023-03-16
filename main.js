355824ee5a9f35659e25b1ad7de32fab433996d0
Maverick <120141146+mavreyn@users.noreply.github.com>
2023-03-15 20:35:20 -0400

    v0.0.54

--- main.js
+++ main.js
@@ -1,18 +1,45 @@
 /*
-    Putting the js in a separate file
-    03.09.2023
+Putting the js in a separate file
+03.09.2023
 */
 
-function updateCursorLoc() {
-    var cursorPos = document.getElementById("user-text").selectionStart;
-    document.getElementById("cursor-loc").innerHTML = cursorPos;
+
+
+const macros = {};
+
+const options = {
+    displayMode: true,
+    throwOnError: false
 }
 
-//split on newlines
-function updateDisp() {
-    var rawUserInput = document.getElementById("user-text").value;
-    var lines = rawUserInput.split("\n");
-    lines = lines.join("<br>");
+var userText = document.getElementById("user-text");
+const mainDisp = document.getElementById("main-display");
 
-    document.getElementById("display-text").innerHTML = lines;
+function mainUpdate() {
+    updateCursorLoc();
+    updateDisplay();
+    updateKaTeX();
+}
+
+//updates the main display using showdown
+function updateDisplay() {
+    var converter = new showdown.Converter();
+    var text = document.getElementById("user-text").value;
+    var html = converter.makeHtml(text);
+    document.getElementById("main-display").innerHTML = html;
+}
+
+//updates the cursor location on a textarea change or click within
+function updateCursorLoc() {
+    var cursorPos = document.getElementById("user-text").selectionStart;
+    document.getElementById("cursor-loc").innerText = cursorPos;
+}
+
+//updates KaTeX still in beta phase
+function updateKaTeX() {
+    var rawUserInput = document.getElementById("user-text").value;
+    var togo = document.getElementById("KaTeX Here");
+    
+    //render the KaTeX with options above
+    katex.render(rawUserInput, togo, options);
 }
