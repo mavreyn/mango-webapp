93ca5d4ed1e9bf46edfe76ca5cc6a9f8dddebd90
Maverick Reynolds <mcreynolds02@gmail.com>
2023-04-01 16:57:07 -0400

    v1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# Changelog
+
+## v1.0 April 1, 2023
+
+- Basic Markdown functionality
+- Automatically detects blocks of text containing mathematical expressions. No delimiters required
+- Scroll sync between user input and render output containers
+- Option to enable debug box
--- settings.js
+++ settings.js
@@ -1,7 +1,40 @@
+const settingsWindow = $("#settings-window");
+settingsWindow.hide();
+const saturationReducer = $("#saturation-reducer");
+saturationReducer.hide();
+
 var useMathNewlines = true;
 
-$("#close-settings-button").click(function() { $("#settings-window").toggle() });
-$("#open-settings-button").click(function() { $("#settings-window").toggle() });
+//toggle settings window
+$("#close-settings-button").click(function() {
+    settingsWindow.hide();
+    saturationReducer.hide();
+});
+$("#open-settings-button").click(function() { 
+    settingsWindow.show();
+    saturationReducer.show();
+});
 
+//add event listener for esc key
+$(document).keydown(function(event) {
+    if (event.key === "Escape") {
+        settingsWindow.hide();
+        saturationReducer.hide();
+    }
+});
+
+//Change which window is visible
+$(document).ready(function() {
+    // Add click event listener to buttons
+    $(".settings-nav-button").click(function() {
+      // Get the ID of the div element to show
+      const divId = $(this).data("divid");
+      // Show the corresponding div element and hide the others
+      $(".settings-section").hide();
+      $("#" + divId).show();
+    });
+  });
+
+//change settings
 $("#cb-toggle-debug").click(function() { $("#debug-box").toggle() });
 $("#cb-use-math-newlines").click(function() { useMathNewlines = $("#cb-use-math-newlines").is(":checked"); updateDisplay(); });
