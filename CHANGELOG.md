a2395432f33c497be75972fc956e41a4fe8cde7a
Maverick Reynolds <mcreynolds02@gmail.com>
2023-04-05 11:51:13 -0400

    isMathBlock Update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,20 @@
 # Changelog
 
-## v1.0 April 1, 2023
+### 1.0.2 April 5, 2023
 
-- Basic Markdown functionality
+- The `isMathBlock()` function will search for at least one word without leading \ or trailing { in the block. If successful, the block is categorized as text
+
+### 1.0.1 April 3, 2023
+
+- Settings window contains editor and display subsections
+- Block splitting will identify multiple newlines as a single delimiter and ignore leading and trailing newlines
+- Expressions typed between $$ will render as inline math
+- Saves textarea to client's local storage
+- Removed div wrappers around each child in main display element
+
+## 1.0 April 1, 2023
+
+- Text not identified as math renders as Markdown
 - Automatically detects blocks of text containing mathematical expressions. No delimiters required
 - Scroll sync between user input and render output containers
-- Option to enable debug box
+- Debug box will show cursor position, math value of current block, and other information
--- main.js
+++ main.js
@@ -3,7 +3,17 @@ Putting the js in a separate file
 03.09.2023
 */
 
-const MATH_THRESHOLD = 0.2; //0 = false 1 = true
+const MATH_THRESHOLD = 0.8; //0 = false 1 = true
+
+//remember to escape out \ with \\
+const SNIPPETS = {
+    "asdf": "ghjk",
+    "yo": "big dawg",
+    "@l": "\\lambda",
+    "IN": "\\in",
+    "Rn": "\\R^n",
+    "===": "\\equiv "
+}
 
 const userText = document.getElementById("user-text");
 const mainDisp = document.getElementById("render-display");
@@ -20,19 +30,29 @@ userText.value = localStorage.getItem('userText');
 
 //---------------------------------------------------------------------//
 
-//compare the percent of non-word-ish characters against the threshold
-function mathValue(b) {
-    var irregularChars = b.match(/[^a-zA-Z#! ]/g);
-    //if no match or the string is part of a header
-    if (irregularChars == null || b.match(/#/)) {
+function sigmoid(z) {
+    return 1 / (1 + Math.exp(-z));
+}
+
+//threshold function to determine if a block of text is math or plain
+//AT THE MOMENT, ONE REGULAR WORD WILL DECLARE NON MATH
+function mathValue(block) {
+    if (block.match(/^#/) || block.length === 1) {
+        // skip headers and first characters
         return 0;
     } else {
-        return irregularChars.length / b.length;
+        //count how many word there are not proceeded with \ or followed with {
+        var matches = block.match(/(?<![\w\\])[a-zA-Z]{2,}(?![\w{])/gm);
+        if (matches === null) {
+            return 1
+        } else {
+            return sigmoid(-matches.length);
+        }
     }
 }
 
-function isMathBlock(b) {
-    return mathValue(b) >= MATH_THRESHOLD;
+function isMathBlock(block) {
+    return mathValue(block) > MATH_THRESHOLD;
 }
 
 
@@ -42,6 +62,7 @@ function mainUpdate() {
     textBlocks = userText.value.trim().split(/\n{2,}/gm);
 
     //render the text, and update the debug box
+    useSnippets();
     updateDisplay();
     updateCursorInfo();
     updateDebugBox();
@@ -117,3 +138,38 @@ function updateDebugBox() {
     document.getElementById("debug-is-math").innerText = isMathBlock(textBlocks[cursorBlock]);
     if (currentSelection != null) { document.getElementById("debug-current-selection").innerText = currentSelection; }
 }
+
+
+//replaces some groups of characters
+//IF CURSOR IS AT THAT POSITION AND LAST CHARACTER TYPED WAS THAT ONE
+function useSnippets() {
+    for (let key in SNIPPETS) {
+        if (userText.value.substring(userText.value.length - key.length) == key) {
+            userText.value = userText.value.replace(key, SNIPPETS[key]);
+        }
+    }   
+}
+
+// http://jsfiddle.net/o5ay42kr/
+
+// var x = /tes(t)/gi;
+// 
+// var y = "I am Testing a thing for my test here";
+// 
+// var matches = y.match(x);
+// 
+// if (matches) {
+//     alert(matches[0].length)
+// }
+// 
+// var keys = [];
+// $('#target').keypress(function(e) {
+//     keys.unshift(e.which);
+//     update();
+// });
+// 
+// function update() {
+//        $('#last')
+//            .prepend($('<li/>').text(String.fromCharCode(keys[0])))
+//            .children(':eq(5)').remove();
+// }
